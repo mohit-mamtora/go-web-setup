fbab3ab5dfb66b1e3fe1f0bd84d51ce6308b68a9
Mohit-Mamtora <mamtora.mohit@gmail.com>
2024-03-27 11:25:52 +0530

    user schema migration

--- migrations/20240311014430-create_user_table.sql
+++ migrations/20240311014430-create_user_table.sql
@@ -1,4 +1,14 @@
 
 -- +migrate Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 -- +migrate Down
+drop table if exists users;
